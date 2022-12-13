f1558df23e8aa8ad2e2f75c9b3f43c3c5db8c760
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-12-13 11:09:57 -0300

    🧮 calculator

---
+++ fundamentals/calculator.rb
@@ -0,0 +1,13 @@
+# Sum Calculator
+
+puts "Enter a number: "
+num1 = gets.chomp()
+
+puts "Enter another number: "
+num2 = gets.chomp()
+
+# String to Integer
+puts (num1.to_i + num2.to_i)
+
+# String to Float 
+puts (num1.to_f + num2.to_f)
