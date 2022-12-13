45f3ea22056b2969b8253c0c14d65af708e2f3da
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-12-13 11:09:33 -0300

    🪢 arrays

---
+++ fundamentals/arrays.rb
@@ -0,0 +1,28 @@
+# Arrays
+
+friends = Array["Kevin", "Karen", "Toddy"]
+friends[1] = "George"
+
+puts friends
+
+types = Array[1, "Kelvin", true]
+
+puts types
+
+week = Array["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Saturday"]
+
+puts today[0]
+
+puts today[1, 2]
+
+boys = Array.new
+
+boys[0] = "Trevor"
+
+puts boys.length()
+
+puts boys.include? "Trevor"
+
+puts boys.reverse()
+
+puts boys.sort()
