060414aacc872077fe35fc733d92ef4ffa5591f8
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-12-13 11:10:26 -0300

    🌀 inputs

---
+++ fundamentals/input.rb
@@ -0,0 +1,11 @@
+# User Input
+
+puts "Enter your name: "
+# Chomp removes \n from the input
+name = gets.chomp() 
+
+puts "Enter your age: "
+age = gets.chomp()
+
+# You also can use , instead of +
+puts("Hello " + name + ", you are " + age)
