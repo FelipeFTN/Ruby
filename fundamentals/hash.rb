ff271af2637825beecee067bdd97f20d7ab268b8
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-12-13 23:02:43 -0300

    ☮ hash

---
+++ fundamentals/hash.rb
@@ -0,0 +1,13 @@
+# Hashes
+
+## Works just like dictionaries
+
+names = ["John", "Tim", "Mary"]
+
+favorite_pizza = {
+  "John" => "Portuguesa",
+  "Mary" => "Catupireza",
+  "Tim" => "Aeromoca",
+}
+
+puts favorite_pizza[names[0]]
